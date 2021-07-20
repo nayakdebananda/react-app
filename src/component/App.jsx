6102f7361c5761edc870917823ba8705f2e7004c
Deba143 <rdebanandanayak@gmail.com>
2021-07-20 09:10:32 +0530

    Added state

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App(){
+function App() {
+    var [count, setCount] = useState(function () {
+        return 0;
+    });
+
+    function increase() {
+
+        setCount(function (prevCount) {
+            return prevCount + 1;
+        });
+    }
+    function decrease() {
+        setCount(count - 1);
+    }
     return <div>
-        <button>+</button>
-        <h1>3</h1>
-        <button>-</button>
+        <button onClick={increase}>+</button>
+        <h1>{count}</h1>
+        <button onClick={decrease}>-</button>
     </div>;
 }
 
